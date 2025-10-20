5fc933dc50422c898dfa3566af35bead0c455ab5
Jerry Lin <jerry_lin@utexas.edu>
2025-10-19 22:26:36 -0500

    Added authentication screens and transition from splash screen

---
+++ Controlio/Controlio/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Controlio
+//
+//  Created by Jerry Lin on 10/19/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
