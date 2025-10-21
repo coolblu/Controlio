fc7e3f0a696c2216b3b60aea2f21fc17a63b078f
bananaRio <yamamasa27@utexas.edu>
2025-10-20 22:49:32 -0500

    added HomeView with a skelton code for TrackpadView

---
+++ Controlio/Controlio/TrackpadView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackpadView.swift
+//  Controlio
+//
+//  Created by Masayuki Yamazaki on 10/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrackpadView : View {
+    var body: some View {
+        Text("Trackpad")
+                    .font(.title)
+                    .navigationTitle("Trackpad")
+                    .navigationBarTitleDisplayMode(.inline)
+    }
+}
